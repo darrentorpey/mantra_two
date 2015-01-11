69bbcfe010d209ae4914e907c44be0ea27b2c048
Darren Torpey <darren.torpey@gmail.com>
2015-01-11 17:30:09 -0500

    Pretty square grid

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,58 @@
 module.exports = function( grunt ) {
-    grunt.loadNpmTasks( 'grunt-eslint' );
-    grunt.loadNpmTasks( 'grunt-6to5' );
-    grunt.loadNpmTasks( 'grunt-contrib-copy' );
-    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+  grunt.loadNpmTasks( 'grunt-eslint' );
+  grunt.loadNpmTasks( 'grunt-6to5' );
+  grunt.loadNpmTasks( 'grunt-contrib-copy' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+  grunt.loadNpmTasks( 'grunt-karma' );
 
-    grunt.initConfig({
-        '6to5': {
-            options: {
-                sourceMap: true,
-                modules: 'amd'
-            },
-            build: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/',
-                    src: [ '**/*.js' ],
-                    dest: 'dist/'
-                }]
-            }
-        },
-        'copy': {
-            'lib': {
-                'files': [{
-                    src: 'lib/**',
-                    dest: 'dist/'
-                }, {
-                    src: 'node_modules/grunt-6to5/node_modules/6to5/browser-polyfill.js',
-                    dest: 'dist/lib/polyfill.js'
-                }]
-            }
-        },
-		'eslint': {
-			options: {
-            	config: 'eslint.json',
-				quiet: true
-			},
-			target: [ 'src/**/*.jsp' ]
-		},
-        'watch': {
-            src: {
-              files: [ 'src/**/*.js', 'lib/**/*.js' ],
-              tasks: [ '6to5', 'copy' ],
-            }
-        }
+  grunt.initConfig({
+    '6to5': {
+      options: {
+        sourceMap: true,
+        modules: 'amd'
+      },
+      build: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: [ '**/*.js' ],
+          dest: 'dist/'
+        }]
+      }
+    },
+
+    'copy': {
+      'lib': {
+        'files': [{
+          src: 'lib/**',
+          dest: 'dist/'
+        }, {
+          src: 'node_modules/grunt-6to5/node_modules/6to5/browser-polyfill.js',
+          dest: 'dist/lib/polyfill.js'
+        }]
+      }
+    },
+
+  	'eslint': {
+  		options: {
+      	config: 'eslint.json',
+  			quiet: true
+  		},
+  		target: [ 'src/**/*.jsp' ]
+  	},
+
+    'watch': {
+      src: {
+        files: [ 'src/**/*.js', 'lib/**/*.js' ],
+        tasks: [ '6to5', 'copy' ],
+      }
+    },
+
+    'karma': {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
+    }
 	});
 
 	grunt.registerTask( 'default', [ 'eslint' ] );
--- src/mantra.js
+++ src/mantra.js
@@ -5,8 +5,6 @@ var canvas = Canvas.create();
 
 document.body.appendChild( canvas.dom );
 
-console.log( 'canvas:', canvas );
-
 var grid = Grid.create({
   width: 8,
   height: 5,
@@ -15,37 +13,62 @@ var grid = Grid.create({
 });
 
 var colorMap = {
-	'W': 'white',
-	'G': 'green',
-	'B': 'blue',
-	'L': 'lightblue',
-}
-
-var addBox = ( { x, y, color } ) => {
-	canvas.drawSquare( { x, y, w: 20, h: 20, style: colorMap[ color ] || color } );
+  '_': null,
+	' ': null,
+  'W': '#DDDDDD',
+  '1': '#F92672',
+  '2': '#66D9EF',
+  '3': '#A6E22E',
+  '4': '#FD971F',
+  '5': '#E6DB74',
+  '6': '#AE81FF',
+  '7': '#DDDDDD'
 };
 
-// canvas.drawSquare( { x: 10, y: 10, w: 20, h: 20, style: 'white' } );
-addBox( { x: 10, y: 10, color: 'white' } );
-addBox( { x: 35, y: 10, color: 'green' } );
-addBox( { x: 60, y: 10, color: 'blue' } );
-addBox( { x: 85, y: 10, color: 'lightblue' } );
-addBox( { x: 110, y: 10, color: 'G' } );
-addBox( { x: 135, y: 10, color: 'G' } );
-
-
 var asciiMap = `
-___W__G__LL_B
-__B_____B____
-_W___W____LW_
+WWWWWWWWWWWWWWWWWWWWWWW
+W                     W
+W  5 3 4 5  6        1W
+W 2              35  1W
+W  5     4 6          W
+W    4  4 2   7       W
+W  2     32           W
+W     6  1   43 1     W
+W              5      W
+W      1  5           W
+W             4       W
+W   1           3     W
+W   4          3  24  W
+W          15    14   W
+W  5            2 3   W
+W    2    3           W
+W                  5  W
+W      11             W
+WWWWWWWWWWWWWWWWWWWWWWW
 `;
 
+var addBox = function( { x, y, color } ) {
+  canvas.drawSquare( { x, y, w: 20, h: 20, style: colorMap[ color ] || color } );
+};
 
-function logGrid() {
+var renderMap = function( start, asciiMap ) {
+  _.each( asciiMap.split( '\n' ), function( row, rowIndex ) {
+    _.each( Array.from( row ), function( cell, colIndex ) {
+      var color = colorMap[ cell ];
+      if ( color ) {
+        addBox( { x: start.x + ( colIndex * 25 ), y: start.y + ( ( rowIndex - 1 ) * 25 ), color: color } );
+      }
+    });
+  });
+};
+
+renderMap( { x: 9, y: 5 }, asciiMap )
+
+var logGrid = function() {
   for ( let tile of grid.getTiles() ) {
     console.log( tile.toString() );
   }
-}
+};
 
 Object.assign( window, {
   canvas,
